26a5300a90585011efddb29dcc80f8757f174c75
RomadovaIrina <113946752+RomadovaIrina@users.noreply.github.com>
2022-10-13 23:06:00 +0300

    Update 1670b.cpp

--- romadova_i_o/to be done/1670b.cpp
+++ romadova_i_o/to be done/1670b.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
-std::string replaced(std::string& s, std::vector<char> &spec) {
-	for (int i = 0; i < s.length(); i += 1) {
-		for (int j = 0; j < spec.size(); j += 1) {
-			if (s[i] == spec[j]) {
-				s[i] = '1';
-			}
-		}
-	}
-	return s;
-}
+
 
 int main()
 {
-	int t, k=0, n=0;
+	int t, k = 0, n = 0;
 	bool flg = 1;
 	std::string s;
+	unsigned char sym;
 	std::cin >> t;
-	for (int j = 0; j < t; j += 1) 
+	for (int j = 0; j < t; j += 1)
 	{
 		std::cin >> n;
 		std::cin >> s;
 		std::cin >> k;
 		std::vector<char> spec(k);
+		std::vector<unsigned char> is_spec(256, '0');
 		for (int l = 0; l < k; l += 1) {
-			std::cin >> spec[l];
+			std::cin >> sym;
+			is_spec[sym] = '1';
 		}
-		s = replaced(s, spec);
 		int counter = 0, max_counter = 0;
 		for (int i = 0; i < s.length(); i += 1) {
-			if (s[i] != '1') {
+			if (is_spec[s[i]] != '1') {
 				counter += 1;
 			}
 			else {
@@ -38,6 +31,7 @@ int main()
 				counter = 1;
 			}
 		}
-		std::cout << max_counter<<std::endl;
+		std::cout << max_counter << std::endl;
 	}
 }
+
