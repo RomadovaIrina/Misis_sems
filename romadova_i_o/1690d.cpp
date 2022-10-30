b43579aae1e27833c09d9b3c1abaa60630579c82
RomadovaIrina <113946752+RomadovaIrina@users.noreply.github.com>
2022-10-30 16:47:30 +0300

    Update 1690d.cpp

--- romadova_i_o/1690d.cpp
+++ romadova_i_o/1690d.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <cmath>
+#include <string>
+
+
 
 int main()
 {
@@ -8,28 +11,29 @@ int main()
 	std::cin >> t;
 	while (t)
 	{
-		int n, k, ans=300000;
-		std::string s;
+		int n, k;
 		std::cin >> n >> k;
+		std::string s;
 		std::cin >> s;
-		std::vector<int> sums(n + 1);
-		for (int i = 1; i <= n; i += 1) {
-			if (s[i-1] == 'W')
-			{
-				sums[i] = sums[i - 1] + 1;
-
-			}
-			else {
-
-				sums[i] = sums[i - 1];
+		int counter = 0, ans = 1000000;
+		for (int i = 0; i < k; i += 1) {
+			if (s[i] == 'W') {
+				counter += 1;
 			}
 		}
-		for (int i = k; i <= n; i += 1)
+		ans = std::min(counter, ans);
+		for (int j = k; j < n; j += 1)
 		{
-			ans = std::min(ans, sums[i] - sums[i - k]);
+			if (s[j] == 'W') {
+				counter += 1;
+			}
+			if (s[j - k] == 'W') {
+				counter -= 1;
+			}
+			ans = std::min(ans, counter);
 		}
 		std::cout << ans << std::endl;
-
 		t -= 1;
 	}
+
 }
