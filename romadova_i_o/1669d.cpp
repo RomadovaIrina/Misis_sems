6492c4b6a434a73fdce5f8dcc1018704cc1f931f
RomadovaIrina <113946752+RomadovaIrina@users.noreply.github.com>
2022-10-31 13:05:27 +0300

    Update 1669d.cpp

--- romadova_i_o/1669d.cpp
+++ romadova_i_o/1669d.cpp
@@ -1,79 +1,43 @@
 #include <iostream>
+#include <cmath>
+#include <array>
 #include <string>
-#include <vector>
-
-std::vector<std::string> f(std::string s)
-{
-	std::string local_s;
-	int n = s.size();
-	std::vector<std::string> A;
-	for (int i = 0; i < n; i++) {
-		char x = s[i];
-		if (x != 'W') {
-			if (x == 'R') {
-				local_s = local_s + "R";
-			}
-			else {
-				local_s = local_s + "B";
-			}
-		}
-		else {
-			if (local_s != "") {
-				A.push_back(local_s);
-				local_s = "";
-			}
-		}
-		if (i == n - 1 && s[i] != 'W') {
-			A.push_back(local_s);
-		}
-	}
-	return A;
-}
-bool is_letter(std::string t, char l) {
-	bool flaf = 0;
-	for (int i = 0; i < t.length(); i += 1) {
-		if (t[i] == l)
-		{
-			flaf = 1;
-			break;
-		}
-	}
-	return flaf;
-
-}
-
-
 int main()
 {
 	int t;
 	std::cin >> t;
-	while (t)
-	{
+	for (int tt = 0; tt < t; tt += 1) {
 		int n;
 		std::string s;
-		std::cin >> n;
-		std::cin >> s;
-		std::vector<std::string> d = f(s);
-		bool flag = 1;
-		for (int i = 0; i < d.size(); i += 1)
-		{
-			if (d[i].length() == 1) {
-				flag = 0;
-				break;
-			}
-			else {
-				bool is_b = is_letter(d[i], 'B'), is_r = is_letter(d[i], 'R');
-				if (!is_b || !is_r) {
-					flag = 0;
+		std::cin >> n >> s;
+		int b = 0, r = 0;
+		int ans = -1;
+		for (int i = 0; i < n; i += 1) {
+			if (s[i] == 'B')
+				b += 1;
+			if (s[i] == 'R')
+				r += 1;
+
+			if (s[i] == 'W') {
+				if ((b==0 && r>0)||(r==0 && b>0)) {
+					ans = 0;
+					break;
 				}
+				if (b + r == 1) {
+					ans = 0;
+					break;
+				}
+				b= 0;
+				r = 0;
 			}
 		}
-		if (flag) {
-			std::cout << "Yes" << std::endl;
+		if ((b == 0 && r > 0) || (r == 0 && b > 0)) {
+			ans = 0;
+			std::cout << "NO" << std::endl;
 		}
-		else {
-			std::cout << "No" << std::endl;
+		else if (ans = -1) {
+			ans = 1;
+			std::cout << "YES" << std::endl;
 		}
-		t -= 1;
 	}
 }
